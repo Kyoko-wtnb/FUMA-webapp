283b2b2e3b4738119081c7f8b1b0e29ba62c2379
Arjen van Bochoven <arjen@bochoven.net>
2016-12-14 15:28:39 +0100

    Add migrations for job and gwas tables

---
+++ database/migrations/2016_12_14_135134_create_job_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateJobTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->increments('jobID');
+            $table->string('email');
+            $table->string('title');
+            $table->string('created_date');
+            $table->string('last_access');
+            $table->string('status');
+            
+            // Add indexes
+            $table->index('email');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('jobs');
+    }
+}
