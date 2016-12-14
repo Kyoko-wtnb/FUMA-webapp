a32bc613e4fd2dff5ed05407a3971ff76e0aa4fc
Arjen van Bochoven <arjen@bochoven.net>
2016-12-14 15:32:15 +0100

    Convert menu links to use subdirectory

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -15,20 +15,20 @@
 <nav class="navbar navbar-inverse navbar-fixed-top">
   <div class="container-fluid">
     <div class="navbar-header" style="padding-left: 30px;">
-      <!-- <a class="navbar-brand" href="/IPGAP/"><span style="color: #1E90FF; font-size: 130%;">IPGAP</span></a> -->
-      <a class="navbar-brand" href="/IPGAP/" style="padding-top: 5px;"><img src="{!! URL::asset('image/logo.png') !!}" height="45px;" width="200px"></a>
+      <!-- <a class="navbar-brand" href="{{ Config::get('app.subdir') }}/"><span style="color: #1E90FF; font-size: 130%;">IPGAP</span></a> -->
+      <a class="navbar-brand" href="{{ Config::get('app.subdir') }}/" style="padding-top: 5px;"><img src="{!! URL::asset('image/logo.png') !!}" height="45px;" width="200px"></a>
     </div>
 
     <div class="collapse navbar-collapse" id="headNav" style="padding-right: 50px;">
       <ul class="nav navbar-nav navbar-right">
         <!-- local_start -->
-        <li class="{{ Request::is('/') ? 'active' : ''}}"><a href="/IPGAP/">Home</a></li>
-        <li class="{{ Request::is('tutorial') ? 'active' : ''}}"><a href="/IPGAP/tutorial">Tutorial</a></li>
-        <!-- <li class="{{ Request::is('GWASresult*') ? 'active' : ''}}"><a href="/IPGAP/GWASresult">GWASresult</a></li> -->
-        <li class="{{ Request::is('snp2gene*') ? 'active' : ''}}"><a href="/IPGAP/snp2gene">SNP2GENE</a></li>
-        <li class="{{ Request::is('gene2func*') ? 'active' : ''}}"><a href="/IPGAP/gene2func">GENE2FUNC</a></li>
-        <li class="{{ Request::is('links') ? 'active' : ''}}"><a href="/IPGAP/links">Links</a></li>
-        <!-- <li class="{{ Request::is('contact') ? 'active' : ''}}"><a href="/IPGAP/contact">Contact</a></li> -->
+        <li class="{{ Request::is('/') ? 'active' : ''}}"><a href="{{ Config::get('app.subdir') }}/">Home</a></li>
+        <li class="{{ Request::is('tutorial') ? 'active' : ''}}"><a href="{{ Config::get('app.subdir') }}/tutorial">Tutorial</a></li>
+        <!-- <li class="{{ Request::is('GWASresult*') ? 'active' : ''}}"><a href="{{ Config::get('app.subdir') }}/GWASresult">GWASresult</a></li> -->
+        <li class="{{ Request::is('snp2gene*') ? 'active' : ''}}"><a href="{{ Config::get('app.subdir') }}/snp2gene">SNP2GENE</a></li>
+        <li class="{{ Request::is('gene2func*') ? 'active' : ''}}"><a href="{{ Config::get('app.subdir') }}/gene2func">GENE2FUNC</a></li>
+        <li class="{{ Request::is('links') ? 'active' : ''}}"><a href="{{ Config::get('app.subdir') }}/links">Links</a></li>
+        <!-- <li class="{{ Request::is('contact') ? 'active' : ''}}"><a href="{{ Config::get('app.subdir') }}/contact">Contact</a></li> -->
         <!-- local_end -->
       </ul>
     </div>
