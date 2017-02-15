b33c607cda3618fb467fe68d2bbfbd5ce5e1011d
Kyoko Watanabe <k.watanabe@vu.nl>
2017-02-15 18:09:48 +0100

    twice a day report scheduled task

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -6,7 +6,7 @@ use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use IPGAP\Http\Controllers\Controller;
 use DB;
-use IPGAP\Jobs\snp2geneProcess;
+use Mail;
 
 class Kernel extends ConsoleKernel
 {
@@ -30,16 +30,80 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')
         //          ->hourly();
 
-        // $schedule->call(function(){
-        //   $newjobs = DB::table('SubmitJobs')->where('status', 'NEW')->get();
-        //   foreach($newjobs as $job){
-        //     $email = $job->email;
-        //     $user = DB::table('users')->where('email', $email)->first();
-        //     $jobID = $job->jobID;
-        //     DB::table('SubmitJobs') -> where('jobID', $jobID)
-        //       -> update(['status'=>'QUEUED']);
-        //     \IPGAP\Http\Controllers\Controller::dispatch(new snp2geneProcess($user, $jobID));
-        //   }
-        // })->everyMinute();
+        $schedule->call(function(){
+          $date = date("Y-m-d");
+          $dateNext = $date;
+          $time = date("H");
+          $report = "today";
+          if ($time < 12){
+            $report = "yesterday";
+            $date = strtotime($date)-60*60*24;
+            $date = date("Y-m-d", $date);
+          } else {
+            $dateNext = strtotime($date)+60*60*24;
+            $dateNext = date("Y-m-d", $dateNext);
+          }
+          $totalNjobs = DB::table('SubmitJobs')->count();
+          $submittedjobs = DB::table('SubmitJobs')->whereColumn([["created_at", ">", $date], ["created_at", "<", $dateNext]])->count();
+          $running = DB::table("SubmitJobs")->where("status", "RUNNING")->get();
+          $runTable = "";
+          if(count($running)>0){
+            $runTable .= "<table><thead><th>jobID</th><th>email<th><th>created_at</th></thead><tbody>";
+            foreach($running as $row){
+              $runtable .= "<tr>";
+              $runTable .= "<td>".$row->jobID."</td>";
+              $runTable .= "<td>".$row->email."</td>";
+              $runTable .= "<td>".$row->created_at."</td>";
+              $runTable .= "</tr>";
+            }
+            $runTable .= "</tbody></table>";
+          }
+          $queued = DB::table("SubmitJobs")->where("status", "QUEUED")->get();
+          $queTable = "";
+          if(count($queued)>0){
+            $queTable .= "<table><thead><th>jobID</th><th>email<th><th>created_at</th></thead><tbody>";
+            foreach($queued as $row){
+              $quetable .= "<tr>";
+              $queTable .= "<td>".$row->jobID."</td>";
+              $queTable .= "<td>".$row->email."</td>";
+              $queTable .= "<td>".$row->created_at."</td>";
+              $queTable .= "</tr>";
+            }
+            $queTable .= "</tbody></table>";
+          }
+          $all = DB::table("SubmitJobs")->select('created_at')->get();
+          $dateStats = [];
+          foreach($all as $a){
+            $tmp = $a->created_at;
+            $tmp = preg_replace("/(.+) \d+:\d+:\d/", '$1', $tmp);
+            if(array_key_exists($tmp, $dateStats)){
+              $dateStats[$tmp] += 1;
+            }else{
+              $dateStats[$tmp] = 1;
+            }
+          }
+
+          $dateavg = 0;
+          foreach ($dateStats as $val){
+            $dateavg += $val;
+          }
+          $dateavg = $dateavg/count($dateStats);
+
+          $data = [
+            'date'=>$date,
+            'totalNjobs'=>$totalNjobs,
+            'running'=>count($running),
+            'runTable'=>$runTable,
+            'queued'=>count($queued),
+            'queTable'=>$queTable,
+            'dateavg'=>$dateavg
+          ];
+
+          Mail::send('emails.JobMonitor', $data, function($m){
+            $m->from('noreply@ctglab.nl', "FUMA web application");
+            $m->to("k.watanabe@vu.nl", "Kyoko Watanabe")->subject("FUMA Monitor Report");
+          });
+        })->everyMinute();
+        // })->twiceDaily(8, 20);
     }
 }
