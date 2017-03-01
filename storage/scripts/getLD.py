1567af7d581c2f3632aefaeb48c3fd8369ab59f9
Kyoko Watanabe <k.watanabe@vu.nl>
2017-03-01 12:33:12 +0100

    getLD.py update

--- storage/scripts/getLD.py
+++ storage/scripts/getLD.py
@@ -12,7 +12,7 @@ from bisect import bisect_left
 # from joblib import Parallel, delayed
 # import multiprocessing
 
-start = time.time()
+starttime = time.time()
 
 # n_cores = multiprocessing.cpu_count()
 # print str(n_cores)+" cores detected"
@@ -23,6 +23,18 @@ if len(sys.argv)<2:
 filedir = sys.argv[1]
 if re.match(".+\/$", filedir) is None:
 	filedir += '/'
+
+##### Return index of a1 which exists in a2 #####
+def ArrayIn(a1, a2):
+	# results = [i for i, x in enumerate(a1) if x in a2]
+	results = np.where(np.in1d(a1, a2))[0]
+	return results
+
+##### return unique element in list #####
+def unique(a):
+	unique = []
+	[unique.append(s) for s in a if s not in unique]
+	return unique
 
 ###################
 # get config files
@@ -58,7 +70,7 @@ KGSNPs = int(param.get('params', 'Incl1KGSNPs')) #1 to add, 0 to not add
 gwasP = float(param.get('params', 'gwasP'))
 maf = float(param.get('params', 'MAF'))
 r2 = float(param.get('params', 'r2'))
-mergeDist = int(param.get('params', 'mergeDist'))
+mergeDist = int(param.get('params', 'mergeDist'))*1000
 MHC = int(param.get('params', 'exMHC')) # 1 to exclude, 0 to not
 extMHC = param.get('params', 'extMHC')
 MHCstart = 29624758 # hg19
@@ -67,17 +79,6 @@ if extMHC != "NA":
     mhc = extMHC.split("-")
     MHCstart = int(mhc[0])
     MHCend = int(mhc[1])
-
-# orcol = param.get('inputfiles', 'orcol')
-# becol = param.get('inputfiles', 'becol')
-# secol = param.get('inputfiles', 'secol')
-#
-# if orcol == "NA":
-#     orcol = None
-# if becol == "NA":
-#     becol = None
-# if secol == "NA":
-#     secol = None
 
 ###################
 # input files
@@ -115,7 +116,10 @@ for i in range(0,len(head)):
 ldout = filedir+"ld.txt"
 snpsout = filedir+"snps.txt"
 annotout = filedir+"annot.txt"
-annovin = filedir+"annoc.input"
+indsigout = filedir+"IndSigSNPs.txt"
+leadout = filedir+"leadSNPs.txt"
+glout = filedir+"GenomicRiskLoci.txt"
+annovin = filedir+"annov.input"
 
 with open(ldout, 'w') as o:
 	o.write("\t".join(["SNP1","SNP2","r2"])+"\n")
@@ -127,6 +131,7 @@ if becol:
 	ohead += "\tbeta"
 if secol:
 	ohead += "\tse"
+ohead += "\tr2\tIndSigSNP\tGenomicLocus\tnearestGene\tdist\tfunc\tCADD\tRDB\tminChrState\tcommonChrState"
 ohead += "\n"
 with open(snpsout, 'w') as o:
 	o.write(ohead)
@@ -194,8 +199,7 @@ for l in gwasf:
 gwasf.close()
 
 gwasfile_chr = np.array(gwasfile_chr)
-for l in gwasfile_chr:
-	print "\t".join(l.astype(str))
+
 refgenome = cfg.get('data', 'refgenome')
 
 def chr_process(ichrom):
@@ -205,20 +209,20 @@ def chr_process(ichrom):
 	if regions is not None:
 		regions_tmp = regions[regions[:,0]==chrom]
 		if len(regions_tmp)==0:
-			return [], [], []
+			return [], [], [], []
 
 	leadSNPs_tmp = None
 	if leadSNPs is not None:
 		leadSNPs_tmp = leadSNPs[leadSNPs[:,1]==chrom]
 		if len(leadSNPs_tmp) == 0 and addleadSNPs == 0:
-			return [], [], []
+			return [], [], [], []
 
 	gwas_in = pd.read_table(gwas, header=None, skiprows=gwasfile_chr[ichrom][1], nrows=gwasfile_chr[ichrom][2])
 	gwas_in = gwas_in.as_matrix()
 
 	if chrom == 6 and MHC == 1:
 		print "Excluding MHC regions ..."
-        gwas_in = gwas_in[(gwas_in[:,poscol].astype(int)<MHCstart) | (gwas_in[:,poscol].astype(int)>MHCend)]
+		gwas_in = gwas_in[(gwas_in[:,poscol].astype(int)<MHCstart) | (gwas_in[:,poscol].astype(int)>MHCend)]
 
 	if regions_tmp:
 		gwas_tmp = np.array()
@@ -226,11 +230,11 @@ def chr_process(ichrom):
 			tmp = gwas_in[(gwas_in[:,poscol].astype(int)>=l[1]) & (gwas_in[:,poscol].astype(int)<=l[2])]
 			if len(tmp)>0:
 				if len(gwas_tmp)>0:
-					gwas_tmp = np.r_(gwas_tmp, tmp)
+					gwas_tmp = np.r_[gwas_tmp, tmp]
 				else:
 					gwas_tmp = tmp
 		if len(gwas_tmp) == 0:
-			return [], [], []
+			return [], [], [], []
 		gwas_in = gwas_tmp
 
 	print str(len(gwas_in))+" SNPs in chromosome "+str(chrom)
@@ -243,7 +247,7 @@ def chr_process(ichrom):
 	ldfile = refgenome+"/"+pop+"ld/"+pop+".chr"+str(chrom)+".ld.gz"
 	annotfile = refgenome+"/"+pop+"/"+"chr"+str(chrom)+".data.txt.gz"
 
-	rsIDset = set(list(gwas_in[:, rsIDcol]))
+	rsIDset = set(gwas_in[:, rsIDcol])
 	checkeduid = []
 
 	if leadSNPs_tmp is not None:
@@ -256,7 +260,6 @@ def chr_process(ichrom):
 			allele = [gwas_in[igwas, refcol], gwas_in[igwas, altcol]]
 			allele.sort()
 			l_uid = ":".join([str(gwas_in[igwas, chrcol]), str(gwas_in[igwas, poscol])]+allele)
-			IndSigSNPs.append([l_uid, l[0], str(ichrom), str(l[2]), str(gwas_in[igwas, pcol])])
 			pos = int(l[2])
 			#check uniq ID
 			tb = tabix.open(annotfile)
@@ -283,6 +286,8 @@ def chr_process(ichrom):
 			maxpos = max(ld_tmp[:,0].astype(int))
 			tb = tabix.open(annotfile)
 			annot_tb = tb.querys(str(chrom)+":"+str(minpos)+"-"+str(maxpos))
+			nonGWASSNPs = 0
+			GWASSNPs = 0
 			for m in annot_tb:
 				if chrom==6 and MHC==1 and int(m[1])>=MHCstart and int(m[1])<=MHCend:
 					continue
@@ -297,12 +302,15 @@ def chr_process(ichrom):
 						allele = [gwas_in[jgwas, refcol], gwas_in[jgwas, altcol]]
 						allele.sort()
 						uid = ":".join([str(gwas_in[jgwas, chrcol]), str(gwas_in[jgwas, poscol])]+allele)
+
 						if uid != m[6]:
 							continue
 
-						ld.append([l_uid, m[6], ld_tmp[ild, 2])
+						ld.append([l_uid, m[6], ld_tmp[ild, 2]])
+
 						if m[6] in checkeduid:
 							continue
+
 						checkeduid.append(m[6])
 						p = str(gwas_in[jgwas, pcol])
 						snp = [m[6], m[4], m[0], m[1], gwas_in[jgwas, refcol], gwas_in[jgwas, altcol], m[5], p]
@@ -314,6 +322,7 @@ def chr_process(ichrom):
 							snp.append(str(gwas_in[jgwas, secol]))
 						canSNPs.append(snp)
 						annot.append([m[6], m[7], m[8]]+m[53:len(m)])
+						GWASSNPs += 1
 					elif KGSNPs==1:
 						ld.append([l_uid, m[6], ld_tmp[ild, 2]])
 						if m[6] in checkeduid:
@@ -328,11 +337,38 @@ def chr_process(ichrom):
 							snp.append("NA")
 						canSNPs.append(snp)
 						annot.append([m[6], m[7], m[8]]+m[53:len(m)])
+						nonGWASSNPs += 1
+
+			IndSigSNPs.append([l_uid, l[0], str(l[1]), str(l[2]), str(gwas_in[igwas, pcol]), str(nonGWASSNPs+GWASSNPs), str(GWASSNPs)])
+
 		if len(gwas_in[gwas_in[:,pcol]<=leadP]) == 0:
-			return ld, canSNPs, annot
+			if len(canSNPs)>0:
+				ld = np.array(ld)
+				canSNPs = np.array(canSNPs)
+				annot = np.array(annot)
+				IndSigSNPs = np.array(IndSigSNPs)
+				IndSigSNPs = IndSigSNPs[IndSigSNPs[:,3].astype(int).argsort()]
+				n = canSNPs[:,3].astype(int).argsort()
+				canSNPs = canSNPs[n]
+				annot = annot[n]
+				return ld, canSNPs, annot, IndSigSNPs
+			else:
+				return [], [], [], []
 
 	if len(gwas_in[gwas_in[:,pcol].astype(float)<=leadP]) == 0:
-		return ld, canSNPs, annot
+		if len(canSNPs)>0:
+			ld = np.array(ld)
+			canSNPs = np.array(canSNPs)
+			annot = np.array(annot)
+			IndSigSNPs = np.array(IndSigSNPs)
+			IndSigSNPs = IndSigSNPs[IndSigSNPs[:,3].astype(int).argsort()]
+			n = canSNPs[:,3].astype(int).argsort()
+			canSNPs = canSNPs[n]
+			annot = annot[n]
+			return ld, canSNPs, annot, IndSigSNPs
+		else:
+			return [], [], [], []
+
 	gwas_in = gwas_in[gwas_in[:,pcol].argsort()]
 	if leadSNPs is None or addleadSNPs == 1:
 		for l in gwas_in:
@@ -350,6 +386,7 @@ def chr_process(ichrom):
 				for m in check_id:
 					if m[6] == l_uid:
 						lead_id = True
+						break
 				if lead_id == False:
 					continue
 				nlead += 1
@@ -367,6 +404,8 @@ def chr_process(ichrom):
 				maxpos = max(ld_tmp[:,0].astype(int))
 				tb = tabix.open(annotfile)
 				annot_tb = tb.querys(str(chrom)+":"+str(minpos)+"-"+str(maxpos))
+				nonGWASSNPs = 0
+				GWASSNPs = 0
 				for m in annot_tb:
 					if chrom==6 and MHC==1 and int(m[1])>=MHCstart and int(m[1])<=MHCend:
 						continue
@@ -397,6 +436,7 @@ def chr_process(ichrom):
 								snp.append(str(gwas_in[jgwas, secol]))
 							canSNPs.append(snp)
 							annot.append([m[6], m[7], m[8]]+m[53:len(m)])
+							GWASSNPs += 1
 						elif KGSNPs==1:
 							ld.append([l_uid, m[6], ld_tmp[ild, 2]])
 							if m[6] in checkeduid:
@@ -411,44 +451,219 @@ def chr_process(ichrom):
 								snp.append("NA")
 							canSNPs.append(snp)
 							annot.append([m[6], m[7], m[8]]+m[53:len(m)])
+							nonGWASSNPs += 1
+				IndSigSNPs.append([l_uid, l[4], str(l[0]), str(l[1]), str(l[5]), str(nonGWASSNPs+GWASSNPs), str(GWASSNPs)])
 
 	if len(canSNPs)>0:
 		ld = np.array(ld)
 		canSNPs = np.array(canSNPs)
 		annot = np.array(annot)
+		IndSigSNPs = np.array(IndSigSNPs)
+		IndSigSNPs = IndSigSNPs[IndSigSNPs[:,3].astype(int).argsort()]
 		n = canSNPs[:,3].astype(int).argsort()
 		canSNPs = canSNPs[n]
 		annot = annot[n]
-		# np.savetxt(filedir+"tmp.ld"+str(chrom)+".txt", ld, delimiter = "\t", fmt="%s")
-		# np.savetxt(filedir+"tmp.snps"+str(chrom)+".txt", canSNPs, delimiter = "\t",  fmt="%s")
-		# np.savetxt(filedir+"tmp.annot"+str(chrom)+".txt", annot, delimiter = "\t",  fmt="%s")
-	return ld, canSNPs, annot
+	return ld, canSNPs, annot, IndSigSNPs
 
 ld = []
-canSNPs = []
+snps = []
 annot = []
 IndSigSNPs = []
 
 for i in range(0, len(gwasfile_chr)):
-	ld_tmp, canSNPs_tmp, annot_tmp = chr_process(i)
-	if len(canSNPs_tmp)>0:
-		if len(canSNPs)>0:
+	ld_tmp, snps_tmp, annot_tmp, IndSigSNPs_tmp = chr_process(i)
+	if len(snps_tmp)>0:
+		if len(snps)>0:
 			ld = np.r_[ld, ld_tmp]
-			canSNPs = np.r_[canSNPs, canSNPs_tmp]
+			snps = np.r_[snps, snps_tmp]
 			annot = np.r_[annot, annot_tmp]
+			IndSigSNPs = np.r_[IndSigSNPs, IndSigSNPs_tmp]
 		else:
 			ld = ld_tmp
-			canSNPs = canSNPs_tmp
+			snps = snps_tmp
 			annot = annot_tmp
+			IndSigSNPs = IndSigSNPs_tmp
 # Parallel(n_jobs=n_cores)(delayed(chr_process)(i) for i in range(0, len(gwasfile_chr)))
-if len(canSNPs) > 0:
-	with open(ldout, 'a') as o:
-		np.savetxt(o, ld, delimiter="\t", fmt="%s")
-	with open(snpsout, 'a') as o:
-		np.savetxt(o, canSNPs, delimiter="\t", fmt="%s")
-	with open(annotout, 'a') as o:
-		np.savetxt(o, annot, delimiter="\t", fmt="%s")
 
+if len(snps)==0:
+	sys.exit("No candidate SNP was identified")
 
+with open(ldout, 'a') as o:
+	np.savetxt(o, ld, delimiter="\t", fmt="%s")
+# with open(snpsout, 'a') as o:
+# 	np.savetxt(o, snps, delimiter="\t", fmt="%s")
+with open(annotout, 'a') as o:
+	np.savetxt(o, annot, delimiter="\t", fmt="%s")
 
-print time.time() - start
+with open(annovin, 'w') as o:
+	for l in snps:
+		o.write("\t".join([l[2], l[3], l[3], l[4], l[5]])+"\n")
+
+leadSNPs = []
+checked = []
+
+IndSigSNPs = IndSigSNPs[IndSigSNPs[:,4].astype(float).argsort()]
+for snp in IndSigSNPs:
+	if snp[1] in checked:
+		continue
+	ldfile = refgenome+'/'+pop+'ld/'+pop+'.chr'+str(snp[2])+'.ld.gz';
+	tb = tabix.open(ldfile)
+	ld_tmp = tb.querys(snp[2]+":"+snp[3]+"-"+snp[3])
+	inSNPs = []
+	inSNPs.append(snp[1])
+	for l in ld_tmp:
+		if float(l[6])<0.1:
+			continue
+		if l[5] in IndSigSNPs[:,1]:
+			checked.append(l[5])
+			inSNPs.append(l[5])
+	leadSNPs.append([snp[0], snp[1], snp[2], snp[3], snp[4], str(len(inSNPs)), ":".join(inSNPs)])
+leadSNPs = np.array(leadSNPs)
+leadSNPs = leadSNPs[np.lexsort((leadSNPs[:,3].astype(int), leadSNPs[:,2].astype(int)))]
+IndSigSNPs = IndSigSNPs[np.lexsort((IndSigSNPs[:,3].astype(int), IndSigSNPs[:,2].astype(int)))]
+
+loci = []
+iloci = 0
+chrom = 0
+inInd = []
+inLead = []
+nonGWASSNPs = []
+GWASSNPs = []
+uid2gl = {}
+for i in range(0, len(leadSNPs)):
+	if i == 0:
+		chrom = int(leadSNPs[i, 2])
+		rsIDs = list(leadSNPs[i,6].split(":"))
+		uid = list(snps[ArrayIn(snps[:,1], rsIDs),0])
+		for s in uid:
+			uid2gl[s] = iloci+1
+		inInd = rsIDs
+		inLead = [leadSNPs[i,1]]
+		n = ArrayIn(snps[:,0], ld[ArrayIn(ld[:,0], uid),1])
+		snps_tmp = snps[n,]
+		nonGWASSNPs += list(snps_tmp[snps_tmp[:,7]=="NA", 0])
+		GWASSNPs += list(snps_tmp[snps_tmp[:,7]!="NA", 0])
+		start = min(snps[n,3].astype(int))
+		end = max(snps[n,3].astype(int))
+		loci.append([str(iloci+1)]+list(leadSNPs[i,range(0,5)])+[str(start), str(end), str(len(nonGWASSNPs)+len(GWASSNPs)), str(len(GWASSNPs)), str(len(inInd)), ":".join(inInd), str(len(inLead)), ":".join(inLead)])
+	elif chrom==int(leadSNPs[i,2]):
+		rsIDs = list(leadSNPs[i,6].split(":"))
+		uid = list(snps[ArrayIn(snps[:,1], rsIDs),0])
+		for s in uid:
+			uid2gl[s] = iloci+1
+		inInd += rsIDs
+		inInd = unique(inInd)
+		inLead += [leadSNPs[i,1]]
+		n = ArrayIn(snps[:,0], ld[ArrayIn(ld[:,0], uid),1])
+		snps_tmp = snps[n,]
+		nonGWASSNPs += list(snps_tmp[snps_tmp[:,7]=="NA", 0])
+		GWASSNPs += list(snps_tmp[snps_tmp[:,7]!="NA", 0])
+		nonGWASSNPs = unique(nonGWASSNPs)
+		GWASSNPs = unique(GWASSNPs)
+		start = min(snps[n,3].astype(int))
+		end = max(snps[n,3].astype(int))
+		if start <= int(loci[iloci][7]) or start-int(loci[iloci][7])<=mergeDist:
+			loci[iloci][6] = str(min(start, int(loci[iloci][6])))
+			loci[iloci][7] = str(max(end, int(loci[iloci][7])))
+			loci[iloci][8] = str(len(nonGWASSNPs)+len(GWASSNPs))
+			loci[iloci][9] = str(len(GWASSNPs))
+			loci[iloci][10] = str(len(inInd))
+			loci[iloci][11] = ":".join(inInd)
+			loci[iloci][12] = str(len(inLead))
+			loci[iloci][13] = ":".join(inLead)
+			if float(leadSNPs[i,4]) < float(loci[iloci][5]):
+				loci[iloci][1] = leadSNPs[i,0]
+				loci[iloci][2] = leadSNPs[i,1]
+				loci[iloci][4] = leadSNPs[i,3]
+				loci[iloci][5] = leadSNPs[i,4]
+		else:
+			iloci += 1
+			inInd = []
+			inLead = []
+			nonGWASSNPs = []
+			GWASSNPs = []
+			rsIDs = list(leadSNPs[i,6].split(":"))
+			uid = list(snps[ArrayIn(snps[:,1], rsIDs),0])
+			for s in uid:
+				uid2gl[s] = iloci+1
+			inInd = rsIDs
+			inLead = [leadSNPs[i,1]]
+			n = ArrayIn(snps[:,0], ld[ArrayIn(ld[:,0], uid),1])
+			snps_tmp = snps[n,]
+			nonGWASSNPs += list(snps_tmp[snps_tmp[:,7]=="NA", 0])
+			GWASSNPs += list(snps_tmp[snps_tmp[:,7]!="NA", 0])
+			start = min(snps[n,3].astype(int))
+			end = max(snps[n,3].astype(int))
+			loci.append([str(iloci+1)]+list(leadSNPs[i,range(0,5)])+[str(start), str(end), str(len(nonGWASSNPs)+len(GWASSNPs)), str(len(GWASSNPs)), str(len(inInd)), ":".join(inInd), str(len(inLead)), ":".join(inLead)])
+	else:
+		chrom = int(leadSNPs[i,2])
+		iloci += 1
+		inInd = []
+		inLead = []
+		nonGWASSNPs = []
+		GWASSNPs = []
+		rsIDs = list(leadSNPs[i,6].split(":"))
+		uid = list(snps[ArrayIn(snps[:,1], rsIDs),0])
+		for s in uid:
+			uid2gl[s] = iloci+1
+		inInd = rsIDs
+		inLead = [leadSNPs[i,1]]
+		n = ArrayIn(snps[:,0], ld[ArrayIn(ld[:,0], uid),1])
+		snps_tmp = snps[n,]
+		nonGWASSNPs += list(snps_tmp[snps_tmp[:,7]=="NA", 0])
+		GWASSNPs += list(snps_tmp[snps_tmp[:,7]!="NA", 0])
+		start = min(snps[n,3].astype(int))
+		end = max(snps[n,3].astype(int))
+		loci.append([str(iloci+1)]+list(leadSNPs[i,range(0,5)])+[str(start), str(end), str(len(nonGWASSNPs)+len(GWASSNPs)), str(len(GWASSNPs)), str(len(inInd)), ":".join(inInd), str(len(inLead)), ":".join(inLead)])
+loci = np.array(loci)
+
+addcol = []
+for i in range(0,len(IndSigSNPs)):
+	addcol.append([str(i+1), str(uid2gl[IndSigSNPs[i,0]])])
+IndSigSNPs = np.c_[addcol, IndSigSNPs]
+
+addcol = []
+for i in range(0,len(leadSNPs)):
+	addcol.append([str(i+1), str(uid2gl[leadSNPs[i,0]])])
+leadSNPs = np.c_[addcol, leadSNPs]
+
+with open(leadout, 'w') as o:
+	o.write("\t".join(["No", "GenomicLocus", "uniqID", "rsID", "chr", "pos", "p","nSNPs", "nGWASSNPs"])+"\n")
+with open(indsigout, 'a') as o:
+	np.savetxt(o, IndSigSNPs, delimiter="\t", fmt="%s")
+
+with open(leadout, 'w') as o:
+	o.write("\t".join(["No", "GenomicLocus", "uniqID", "rsID", "chr", "pos", "p","nIndSigSNPs", "IndSigSNPs"])+"\n")
+with open(leadout, 'a') as o:
+	np.savetxt(o, leadSNPs, delimiter="\t", fmt="%s")
+
+with open(glout, 'w') as o:
+	o.write("\t".join(["GenomicLocus", "uniqID", "rsID", "chr", "pos", "p", "start", "end", "nSNPs", "nGWASSNPs", "nIndSigSNPs", "IndSigSNPs", "nLeadSNPs", "LeadSNPs"])+"\n")
+with open(glout, 'a') as o:
+	np.savetxt(o, loci, delimiter="\t", fmt="%s")
+
+###################
+# ANNOVAR
+###################
+annov = cfg.get('annovar', 'annovdir')
+humandb = cfg.get('annovar', 'humandb')
+annovout = filedir+"annov"
+os.system(annov+"/annotate_variation.pl -out "+annovout+" -build hg19 "+annovin+" "+humandb+"/ -dbtype ensGene")
+annov1 = filedir+"annov.variant_function"
+annov2 = filedir+"annov.txt"
+os.system(os.path.dirname(os.path.realpath(__file__))+"/annov_geneSNPs.pl "+annov1+" "+annov2)
+annov = pd.read_table(annov2, sep="\t")
+annov = annov.as_matrix()
+
+###################
+# snps annotation
+###################
+addcol = [] #r2, IndSigSNP, GenomicLocus, nearestGene, dist, func, CADD, RDB, minChrState, commonChrState
+for l in snps:
+	tmp = list(ld[ld[:,1]==l[0] & float(ld[:,2])==max(ld[ld[:,1]==l[0],2].astype(float))])
+	r2 = tmp[2]
+	IndSigSNP = snps[snps[:,0]==tmp[0],1]
+	GenomicLocus = IndSigSNPs[IndSigSNPs[:,2]==temp[0],1]
+	tmp = annov[annov[:,0]==l[0] & annov[]]
+
+print time.time() - starttime
