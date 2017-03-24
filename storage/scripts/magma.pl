3b3d99045b862786c64aff5a601274153c2c69a0
Kyoko Watanabe <k.watanabe@vu.nl>
2017-03-24 17:13:40 +0100

    MAGMA exclude MHC

--- storage/scripts/magma.pl
+++ storage/scripts/magma.pl
@@ -4,48 +4,67 @@ use warnings;
 use Config::Simple;
 use File::Basename;
 
+##### check arguments #####
 die "ERROR: not enought arguments\nUSAGE./magma.pl <filedir>\n" if(@ARGV<1);
 
 my $filedir = $ARGV[0];
 $filedir .= '/' unless($filedir =~ /\/$/);
 
+##### get config files #####
 my $dir = dirname(__FILE__);
 my $cfg = new Config::Simple($dir.'/app.config');
 my $params = new Config::Simple($filedir.'params.config');
 
-# my $N = $ARGV[1];
+##### get prams ######
 my $N = $params->param('params.N');
 my $Ncol = $params->param('params.Ncol');
 $N = undef if($N eq "NA");
 
-# my $pop = $ARGV[2];
 my $pop = $params->param('params.pop');
 if($pop =~ /\+/){
 	#$pop =~ s/\(|\)//g;
 	($pop) = split(/\+/, $pop);
 }
 $pop = lc($pop);
-
 print "Population: $pop\n";
+
+my $MHC = $params->param('params.exMHC'); # 1 to exclude, 0 to not
+my $extMHC = $params->param('params.extMHC');
+my $MHCstart = 29624758;
+my $MHCend = 33160276;
+unless($extMHC eq "NA"){
+	my @temp = split(/-/, $extMHC);
+	$MHCstart = $temp[0];
+	$MHCend = $temp[1];
+}
 
 my $outSNPs = $filedir."input.snps";
 my $magmain = $filedir."magma.input";
 my $magmafiles = $cfg->param('magma.magmafiles');
 my $ref = "$magmafiles/g1000_".lc($pop)."_146";
-
 my $magma = $cfg->param('magma.magmadir');
+my $temp = $filedir."temp.txt";
+
 if(defined $N){
-	system "awk 'NR>=2' $outSNPs | cut -f 5,6 | sort -u -k 1,1 > $magmain";
-	system "$magma/magma --bfile $ref --pval $magmain N=$N --gene-annot $magmafiles/ENSG.w0.$pop.genes.annot --out $filedir"."magma";
+	if($MHC eq "1"){
+		system "awk 'NR>=2' $outSNPs | awk '{if(\$1!=6){print \$0}else if(\$2<$MHCstart || \$2>$MHCend){print \$0}}' | cut -f 5,6 | sort -u -k 1,1 > $magmain"
+	}else{
+		system "awk 'NR>=2' $outSNPs | cut -f 5,6 | sort -u -k 1,1 > $magmain";
+	}
+	#system "$magma/magma --bfile $ref --pval $magmain N=$N --gene-annot $magmafiles/ENSG.w0.$pop.genes.annot --out $filedir"."magma";
 }else{
 	open(TMP, $outSNPs);
 	my $head = <TMP>;
 	close TMP;
 	my @head = split(/\s+/, $head);
 	$Ncol = $#head+1;
-	system "awk 'NR>=2' $outSNPs | cut -f 5,6,$Ncol | sort -u -k 1,1 > $magmain";
+	if($MHC eq "1"){
+		system "awk 'NR>=2' $outSNPs | awk '{if(\$1!=6){print \$0}else if(\$2<$MHCstart || \$2>$MHCend){print \$0}}' | cut -f 5,6,$Ncol | sort -u -k 1,1 > $magmain"
+	}else{
+		system "awk 'NR>=2' $outSNPs | cut -f 5,6,$Ncol | sort -u -k 1,1 > $magmain";
+	}
 	open(IN, "$magmain");
-	open(OUT, ">temp.txt");
+	open(OUT, ">$temp");
 	while(<IN>){
 		my @line = split(/\s/, $_);
 		$line[2] = int($line[2]);
@@ -53,23 +72,23 @@ if(defined $N){
 	}
 	close OUT;
 	close IN;
-	system "mv temp.txt $magmain";
-	system "$magma/magma --bfile $ref --pval $magmain ncol=3 --gene-annot $magmafiles/ENSG.w0.$pop.genes.annot --out $filedir"."magma";
+	system "mv $temp $magmain";
+	#system "$magma/magma --bfile $ref --pval $magmain ncol=3 --gene-annot $magmafiles/ENSG.w0.$pop.genes.annot --out $filedir"."magma";
 }
 
 unless(-e $filedir."magma.genes.out"){
 	die "MAGMA ERROR";
 }
 
-system "sed 's/ \\+/\\t/g' ".$filedir."magma.genes.out > $filedir"."temp.txt";
-system "mv ".$filedir."temp.txt ".$filedir."magma.genes.out";
+#system "sed 's/ \\+/\\t/g' ".$filedir."magma.genes.out > $filedir"."temp.txt";
+#system "mv ".$filedir."temp.txt ".$filedir."magma.genes.out";
 
 # MAGMA gene set
-system "$magma/magma --gene-results $filedir"."magma.genes.raw --set-annot $magmafiles/magma_GS.txt --out $filedir"."magma";
+#system "$magma/magma --gene-results $filedir"."magma.genes.raw --set-annot $magmafiles/magma_GS.txt --out $filedir"."magma";
 # MAGMA gene expression
-system "$magma/magma --gene-results $filedir"."magma.genes.raw --gene-covar $magmafiles/gtex.avg.log2RPKM.ts.txt onesided=greater condition=Average --out $filedir"."magma_exp";
-system "$magma/magma --gene-results $filedir"."magma.genes.raw --gene-covar $magmafiles/gtex.avg.log2RPKM.ts.general.txt onesided=greater condition=Average --out $filedir"."magma_exp_general";
+#system "$magma/magma --gene-results $filedir"."magma.genes.raw --gene-covar $magmafiles/gtex.avg.log2RPKM.ts.txt onesided=greater condition=Average --out $filedir"."magma_exp";
+#system "$magma/magma --gene-results $filedir"."magma.genes.raw --gene-covar $magmafiles/gtex.avg.log2RPKM.ts.general.txt onesided=greater condition=Average --out $filedir"."magma_exp_general";
 #remove extra magma log files
-system "rm $filedir"."magma*.log";
+#system "rm $filedir"."magma*.log";
 
-system "Rscript $dir/magma_gene.R $filedir";
+#system "Rscript $dir/magma_gene.R $filedir";
