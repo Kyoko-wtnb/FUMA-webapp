84c5f1e8e0998f7ac777dd81e08e7e71a5e816f5
Kyoko Watanabe <k.watanabe@vu.nl>
2017-03-24 13:59:46 +0100

    update page

--- resources/views/pages/updates.blade.php
+++ resources/views/pages/updates.blade.php
@@ -16,25 +16,32 @@
       </tr>
     </thead>
     <tbody>
-      <tr>
-        <td>17 Mar 2017</td>
-        <td>v1.1.0</td>
-        <td>In GENE2FUNC, in addition to enrichment test of differentialy expressed genes, enrichment of genes expressed in each tissue is also tested.
-          Details are in the <a href="{{ Config::get('app.subdir') }}/tutorial#g2fOutputs">GENE2FUNC Outputs</a> section of the tutorial.
-        </td>
-      </tr>
-      <tr>
-        <td>17 Mar 2017</td>
-        <td>v1.1.0</td>
-        <td>In SNP2GENE, MAGMA tissue expression analyses was added to "Genome wide plot".
-          Details are in the <a href="{{ Config::get('app.subdir') }}/tutorial#outputs">SNP2GENE Outputs</a> section of the tutorial.
-        </td>
-      </tr>
-      <tr>
-        <td>21 Feb 2017</td>
-        <td>v1.0.0</td>
-        <td>The first version was freezed.</td>
-      </tr>
+		<tr>
+	      <td>24 Mar 2017</td>
+	      <td>v1.1.1</td>
+	      <td>SNPs filtering with functional annotation for gene mapping is now reflected in the regional plot with annotations.
+			  Details are described at the bottom of the page of regional plot with annotations.
+		  </td>
+	    </tr>
+		<tr>
+			<td>17 Mar 2017</td>
+			<td>v1.1.0</td>
+			<td>In GENE2FUNC, in addition to enrichment test of differentialy expressed genes, enrichment of genes expressed in each tissue is also tested.
+			  Details are in the <a href="{{ Config::get('app.subdir') }}/tutorial#g2fOutputs">GENE2FUNC Outputs</a> section of the tutorial.
+			</td>
+		</tr>
+		<tr>
+			<td>17 Mar 2017</td>
+			<td>v1.1.0</td>
+			<td>In SNP2GENE, MAGMA tissue expression analyses was added to "Genome wide plot".
+			  Details are in the <a href="{{ Config::get('app.subdir') }}/tutorial#outputs">SNP2GENE Outputs</a> section of the tutorial.
+			</td>
+		</tr>
+		<tr>
+			<td>21 Feb 2017</td>
+			<td>v1.0.0</td>
+			<td>The first version was freezed.</td>
+		</tr>
     </tbody>
   </table>
 </div>
