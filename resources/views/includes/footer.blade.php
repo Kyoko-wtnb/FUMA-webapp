8355cd8444d7553a80ef2f4eed7230fc6077d388
Kyoko Watanabe <k.watanabe@vu.nl>
2017-04-07 13:27:12 +0200

    google analytics

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -14,3 +14,4 @@
 <script type="text/javascript" src="{!! URL::asset('js/sweetalert.min.js') !!}"></script>
 <link rel="stylesheet" href="{!! URL::asset('css/sweetalert.css') !!}">
 <script type="text/javascript" src="{!! URL::asset('js/fuma.js') !!}"></script>
+<script src="{!! URL::asset('js/google_analytics.js') !!}"></script>
