be2ac3fb41d6155b626d6e7b955178a2515b6a22
Kyoko Watanabe <k.watanabe@vu.nl>
2017-05-10 10:25:16 +0200

    annotPlot bug fixed

--- resources/views/pages/annotPlot.blade.php
+++ resources/views/pages/annotPlot.blade.php
@@ -56,7 +56,7 @@ $(document).ready(function(){
 		  Chr15cells: Chr15cells
 	  },
 	  beforeSend: function(){
-		  $("#loading").html('<span style="height:auto; color:grey;"><i class="fa fa-spinner fa-pulse fa-5x fa-fw"></i><br/>Loading...</span>');
+		  $("#load").append('<span style="color:grey;"><i class="fa fa-spinner fa-pulse fa-5x fa-fw"></i><br/>Loading ...</span><br/>');
 	  },
 	  success: function(data){
 		  plotData = JSON.parse(data.replace(/NaN/g, "-1"));
@@ -80,7 +80,7 @@ $(document).ready(function(){
 			  },
 			  success: function(data){
 				  genes = JSON.parse(data);
-				  $('#loading').html("");
+				  $('#load').html("");
 			  },
 			  complete: function(){
 				Plot();
@@ -1078,9 +1078,9 @@ $(document).ready(function(){
 						.attr("fill", "grey");
 
 					svg.append("text").attr("text-anchor", "start")
-						.attr("transform", "translate(10,"+(ciTop+cur_height+5)+")")
+						.attr("transform", "translate(10,"+(ciTop+cur_height+5*i+2)+")")
 						.text(types.join(" "))
-						.style("font-size", "10px").style("font-family", "sans-serif");
+						.style("font-size", "8.5px").style("font-family", "sans-serif");
 					svg.append("g").attr("class", "y axis").call(yAxis)
 						.selectAll('text').attr("transform", "translate(-5,0)").style('font-size', '11px');
 					if(i==plotData.citypes.length-1 && plotData["cieid"].length==0){
@@ -1642,59 +1642,59 @@ function ImgDown(id, type){
 @section('content')
 <canvas id="canvas" style="display:none;"></canvas>
 
+<div id="test"></div>
 <!-- <h3>Annotplot head</h3> -->
 <br/><br/>
 <div class="container">
-<div class="row">
-  <div class="col-md-9 col-xs-9 col-sm-9">
-    <div id='title' style="text-align: center;"><h4>Regional plot</h4></div>
-    <span class="info"><i class="fa fa-info"></i>
-      For SNPs colored grey in the plots of GWAS P-value, CADD, RegulomeDB score and eQTLs, please refer the legend at the bottom of the plot.
-    </span><br/>
-    <a id="plotclear" style="position: absolute;right: 30px;">Clear</a><br/>
-    Download the plot as
-    <button class="btn btn-xs ImgDown" onclick='ImgDown("annotPlot","png");'>PNG</button>
-    <button class="btn btn-xs ImgDown" onclick='ImgDown("annotPlot","jpeg");'>JPG</button>
-    <button class="btn btn-xs ImgDown" onclick='ImgDown("annotPlot","svg");'>SVG</button>
-    <button class="btn btn-xs ImgDown" onclick='ImgDown("annotPlot","pdf");'>PDF</button>
+	<div class="row">
+	  <div class="col-md-9 col-xs-9 col-sm-9">
+	    <div id='title' style="text-align: center;"><h4>Regional plot</h4></div>
+	    <span class="info"><i class="fa fa-info"></i>
+	      For SNPs colored grey in the plots of GWAS P-value, CADD, RegulomeDB score and eQTLs, please refer the legend at the bottom of the plot.
+	    </span><br/>
+		<a id="plotclear" style="position: absolute;right: 30px;">Clear</a><br/>
+	    Download the plot as
+	    <button class="btn btn-xs ImgDown" onclick='ImgDown("annotPlot","png");'>PNG</button>
+	    <button class="btn btn-xs ImgDown" onclick='ImgDown("annotPlot","jpeg");'>JPG</button>
+	    <button class="btn btn-xs ImgDown" onclick='ImgDown("annotPlot","svg");'>SVG</button>
+	    <button class="btn btn-xs ImgDown" onclick='ImgDown("annotPlot","pdf");'>PDF</button>
 
-    <form method="post" target="_blank" action="{{ Config::get('app.subdir') }}/snp2gene/imgdown">
-      <input type="hidden" name="_token" value="{{ csrf_token() }}">
-      <input type="hidden" name="dir" id="annotPlotDir" val=""/>
-      <input type="hidden" name="id" id="annotPlotJobID" val=""/>
-      <input type="hidden" name="data" id="annotPlotData" val=""/>
-      <input type="hidden" name="type" id="annotPlotType" val=""/>
-      <input type="hidden" name="fileName" id="annotPlotFileName" val=""/>
-      <input type="submit" id="annotPlotSubmit" class="ImgDownSubmit"/>
-    </form>
-	<div id="loading"></div>
-	<div id="annotPlot"></div>
-    <br/>
-    <div id="RDBlegend"></div>
-	*External link to RegulomeDB from SNP table (when one of the SNPs is clicked) will open a new tab.
-	rsID does not always match since RegulomeDB used dbSNP build 141 (the rsID in FUMA is dbSNP build 146).
-	Genomic position (bp on hg19) shown in the link of RegulomeDB is the position shown in the SNP table - 1, since RegulomeDB used 0 based corrdinate.
-    <br/>
-    <div id="EIDlegend"></div>
-    <br/>
-    <div id="SNPlegend">
-      <h4>SNPs colored grey in the plots</h4>
-      <strong>GWAS P-value</strong>: SNPs which are not in LD of any of significant independent lead SNPs in the selected region are colored grey.<br/>
-      <strong>CADD score</strong>: Only SNPs which are in LD of any of significant independet lead SNPs are displayed in the plot.
-      Of those SNPs, SNPs which did not used for mapping (SNPs that were filtered by user defined parameters) are colored grey.
-      When both positional and eQTL mappings were performed, only SNPs which were not used either of them are colored grey.<br/>
-      <strong>RegulomeDB score</strong>: Same as CADD score.<br/>
-      <strong>eQTLs</strong>: When eQTL mapping was performed and eQTLs exist in the selected region, all eQTLs with user defined P-value threshold and tissue types are displayed.
-      Of those eQTLs, eQTLs which did not used for eQTL mapping (eQTLs that were filtered by user defined parameters) are colored grey.<br/>
-    </div>
-  </div>
-  <div class="col-md-3 col-xs-3 col-sm-3" style="text-align: center;">
-    <h4>SNP annotations</h4>
-    <div id="annotTable">
-      click any SNP on the plot</br>
-    </div>
-  </div>
-</div>
+	    <form method="post" target="_blank" action="{{ Config::get('app.subdir') }}/snp2gene/imgdown">
+	      <input type="hidden" name="_token" value="{{ csrf_token() }}">
+	      <input type="hidden" name="dir" id="annotPlotDir" val=""/>
+	      <input type="hidden" name="id" id="annotPlotJobID" val=""/>
+	      <input type="hidden" name="data" id="annotPlotData" val=""/>
+	      <input type="hidden" name="type" id="annotPlotType" val=""/>
+	      <input type="hidden" name="fileName" id="annotPlotFileName" val=""/>
+	      <input type="submit" id="annotPlotSubmit" class="ImgDownSubmit"/>
+	    </form>    <div id="annotPlot"></div>
+	    <br/>
+		<div id="load" style="text-align:center;"></div>
+	    <div id="RDBlegend"></div>
+		*External link to RegulomeDB from SNP table (when one of the SNPs is clicked) will open a new tab.
+		rsID does not always match since RegulomeDB used dbSNP build 141 (the rsID in FUMA is dbSNP build 146).
+		Genomic position (bp on hg19) shown in the link of RegulomeDB is the position shown in the SNP table - 1, since RegulomeDB used 0 based corrdinate.
+	    <br/>
+	    <div id="EIDlegend"></div>
+	    <br/>
+	    <div id="SNPlegend">
+	      <h4>SNPs colored grey in the plots</h4>
+	      <strong>GWAS P-value</strong>: SNPs which are not in LD of any of significant independent lead SNPs in the selected region are colored grey.<br/>
+	      <strong>CADD score</strong>: Only SNPs which are in LD of any of significant independet lead SNPs are displayed in the plot.
+	      Of those SNPs, SNPs which did not used for mapping (SNPs that were filtered by user defined parameters) are colored grey.
+	      When both positional and eQTL mappings were performed, only SNPs which were not used either of them are colored grey.<br/>
+	      <strong>RegulomeDB score</strong>: Same as CADD score.<br/>
+	      <strong>eQTLs</strong>: When eQTL mapping was performed and eQTLs exist in the selected region, all eQTLs with user defined P-value threshold and tissue types are displayed.
+	      Of those eQTLs, eQTLs which did not used for eQTL mapping (eQTLs that were filtered by user defined parameters) are colored grey.<br/>
+	    </div>
+	  </div>
+	  <div class="col-md-3 col-xs-3 col-sm-3" style="text-align: center;">
+	    <h4>SNP annotations</h4>
+	    <div id="annotTable">
+	      click any SNP on the plot</br>
+	    </div>
+	  </div>
+	</div>
 </div>
 
 <br/><br/>
