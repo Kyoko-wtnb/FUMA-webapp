6b88762c43b9c596528f4bded3e3e3d7c09a6c5a
Baldur van Lew <bvanlew@lkeb-18-bvl1.lumcnet.prod.intern>
2021-05-09 16:43:26 +0200

    Add a user example to the roles seeder

--- database/seeds/RolesAndPermissionsSeeder.php
+++ database/seeds/RolesAndPermissionsSeeder.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
+use Illuminate\Support\Facades\DB;
 
 class RolesAndPermissionsSeeder extends Seeder
 {
@@ -24,5 +25,13 @@ class RolesAndPermissionsSeeder extends Seeder
             ->givePermissionTo('Queue jobs');
         $guestRole = Role::create(['guard_name' => 'web', 'name' => 'GuestRunner'])
             ->givePermissionTo('Queue jobs');
+
+        // Assign at least one user to have the Admin role
+        // This assumes that the Admin role has id 1 (role_id)
+        // You need to insert an existing user id at model_id
+        // and uncomment the code
+        //DB::table('model_has_roles')->insert(
+        //    ['role_id'=>1, 'model_type'=>'fuma\User', 'model_id'=>/** Replace this comment by an existing user id*/]
+        //);
     }
 }
