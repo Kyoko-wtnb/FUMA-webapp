ca5f7aa47a5334e4f16be8d6ae4fe2fbf7f5f987
root <root@pvsge103.labs.vu.nl>
2022-03-25 10:51:14 +0100

    Add artisan commands for stale job maintenance

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,9 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        // Commands\Inspire::class,
+        Commands\Inspire::class,
+	Commands\ListStaleJobs::class,
+        Commands\RestartStaleJobs::class,
     ];
 
     /**
