ac53a5545654e88cb20c7c222fe616865c7efed8
douglasw <d.p.wightman@vu.nl>
2022-12-02 12:37:36 +0100

    FixUpdateandLinksPage

--- resources/views/pages/links.blade.php
+++ resources/views/pages/links.blade.php
@@ -365,7 +365,7 @@ var loggedin = "{{ Auth::check() }}";
 					Info: <a href="https://www.ebi.ac.uk/gwas/" target="_blank">https://www.ebi.ac.uk/gwas/</a><br/>
 					Data: <a href="https://www.ebi.ac.uk/gwas/downloads" target="_blank">https://www.ebi.ac.uk/gwas/downloads</a>
 				</td>
-				<td>18 Sept 2021</td>
+				<td>02 Dec 2022</td>
 				<td>
 					MacArthur, J., et al. 2016. The new NHGRI-EBI Catalog of published genome-wide association studies (GWAS Catalog). <i>Nucleic Acids Res.</i> pii:gkw1133.<br/>
 					<a href="https://www.ncbi.nlm.nih.gov/pubmed/27899670" target="_blank">PMID:27899670</a>
