bdf7f0c65eba748689cb572495caae99f8b96fcb
Tasos Mitsigkolas <tasos1109@gmail.com>
2023-03-14 15:47:13 +0100

    Relations
    +User relation
    +Tool relation
    +ToolParameter relation

--- app/Models/ToolParameter.php
+++ app/Models/ToolParameter.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Model;
 
 class ToolParameter extends Model
@@ -10,4 +11,14 @@ class ToolParameter extends Model
     use HasFactory;
 
     protected $table = 'tools_parameters';
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function tool(): BelongsTo
+    {
+        return $this->belongsTo(Tool::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,8 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
@@ -41,4 +43,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function tools(): HasMany
+    {
+        return $this->hasMany(Tool::class);
+    }
 }
