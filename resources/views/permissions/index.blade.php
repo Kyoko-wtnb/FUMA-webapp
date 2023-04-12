7cd0f70a0cb53822d2857cb0873ff6eb9626eb2e
Baldur van Lew <baldurvanlew@gmail.com>
2023-04-12 15:51:33 +0200

    Fix Permissions blade header
    and correct type for analytics script

--- resources/views/permissions/index.blade.php
+++ resources/views/permissions/index.blade.php
@@ -1,8 +1,5 @@
 @extends('layouts.master')
-@section('head')
-<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-@stop
+
 @section('title', '| Permissions')
 
 @section('head')
