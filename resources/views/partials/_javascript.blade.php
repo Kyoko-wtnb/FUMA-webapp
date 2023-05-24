355d504eead5ee285d3c07e6320448d5b3a92b5d
Anastasios Mitsigkolas <tasos1109@gmail.com>
2023-05-24 11:40:23 +0200

    Update jquery-ui.min.js

--- resources/views/partials/_javascript.blade.php
+++ resources/views/partials/_javascript.blade.php
@@ -7,12 +7,12 @@
 {{-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script> --}}
 {{-- uptated to: --}}
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.13.2/jquery-ui.min.js"></script>
 
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 {{-- updated to: --}}
 {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script> --}}
 
-<script src="{!! URL::asset('js/google_analytics.js') !!}"></script>
 <script type="text/javascript" src="{!! URL::asset('js/fuma.js') !!}"></script>
 <script type="text/javascript" src="{!! URL::asset('js/HoldOn.min.js') !!}"></script>
 <script type="text/javascript" src="{!! URL::asset('js/sweetalert.min.js') !!}"></script>
