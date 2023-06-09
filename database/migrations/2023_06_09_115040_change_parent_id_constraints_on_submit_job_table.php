06cf0bc164c24b99b7258bf23217644e2b3ec1aa
Anastasios Mitsigkolas <tasos1109@gmail.com>
2023-06-09 15:14:20 +0200

    More migrations

---
+++ database/migrations/2023_06_09_115040_change_parent_id_constraints_on_submit_job_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('SubmitJobs', function (Blueprint $table) {
+            $table->dropForeign(['parent_id']);
+
+            $table->foreign('parent_id')
+                ->nullable()
+                ->references('jobID')
+                ->on('SubmitJobs')
+                ->onUpdate('cascade')
+                ->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('SubmitJobs', function (Blueprint $table) {
+            //
+        });
+    }
+};
