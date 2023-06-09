5a9ae4d765084a1fde6307ca205c6f41455cf4ee
Anastasios Mitsigkolas <tasos1109@gmail.com>
2023-06-09 09:52:09 +0200

    new migrations

---
+++ database/migrations/2023_06_06_155037_change__submit_jobs_table_timestamps_columns_to_nullable.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('SubmitJobs', function (Blueprint $table) {
+            $table->timestamp('started_at')->nullable()->change();
+            $table->timestamp('completed_at')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('SubmitJobs', function (Blueprint $table) {
+            //
+        });
+    }
+};
