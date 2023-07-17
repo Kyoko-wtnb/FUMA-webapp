051458a38d9cd9635266603bc5f013bcb8ec4e70
Anastasios Mitsigkolas <tasos1109@gmail.com>
2023-07-17 17:41:10 +0200

    FumaController refactors
    Refactors, cleanups, commonly used functions have been moved to myFile class.

--- app/Http/Controllers/FumaController.php
+++ app/Http/Controllers/FumaController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use App\CustomClasses\DockerApi\DockerNamesBuilder;
+use App\CustomClasses\myFile;
 use App\Models\SubmitJob;
 use App\Models\User;
 
@@ -33,38 +34,10 @@ class FumaController extends Controller
         $prefix = $request->input('prefix');
         $fin = $request->input('infile');
         $cols = $request->input('header');
-        $cols = explode(":", $cols);
-        $filedir = config('app.temp_abs_path_to_jobs') . '/' . $prefix . '/' . $id . '/';
-        $f = $filedir . $fin;
-        if (file_exists($f)) {
-            $file = fopen($f, 'r');
-            $all_rows = array();
-            $head = fgetcsv($file, 0, "\t");
-            $index = array();
-            foreach ($cols as $c) {
-                if (in_array($c, $head)) {
-                    $index[] = array_search($c, $head);
-                } else {
-                    $index[] = -1;
-                }
-            }
-            while ($row = fgetcsv($file, 0, "\t")) {
-                $temp = [];
-                foreach ($index as $i) {
-                    if ($i == -1) {
-                        $temp[] = "NA";
-                    } else {
-                        $temp[] = $row[$i];
-                    }
-                }
-                $all_rows[] = $temp;
-            }
-            $json = (array('data' => $all_rows));
 
-            echo json_encode($json);
-        } else {
-            echo '{"data":[]}';
-        }
+        $file_path = config('app.jobdir') . '/jobs/' . $id . '/' . $fin;
+
+        return myFile::processCsvDataWithHeaders($file_path, $cols);
     }
 
     public function DTfileServerSide(Request $request)
@@ -89,205 +62,36 @@ class FumaController extends Controller
         $out = shell_exec($cmd);
         echo $out;
 
-
         // TODO: The following code (result of chatgpt) with some modifications could be used as a drop in replacement
         // for the dt.py code and thus to replace the docker container.
-
-        // function read_csv($filedir, $filename) {
-        //     $data = array();
-        //     $file = fopen($filedir.$filename, "r");
-        //     $header = fgetcsv($file, 0, "\t");
-        
-        //     while ($row = fgetcsv($file, 0, "\t")) {
-        //         array_push($data, $row);
-        //     }
-        
-        //     fclose($file);
-        //     return array("header" => $header, "data" => $data);
-        // }
-        
-        // function dt($filedir, $filename, $draw, $headers, $sort_col, $sort_dir, $start, $length, $search = null) {
-        //     if ($search) {
-        //         $search = strtolower($search);
-        //     }
-        
-        //     $cols = explode(":", $headers);
-        //     $fin = read_csv($filedir, $filename)["data"];
-        //     $header = read_csv($filedir, $filename)["header"];
-        //     $hind = array_map(function($x) use ($header) { return array_search($x, $header); }, $cols);
-        //     $fin = array_map(function($row) use ($hind) { return array_intersect_key($row, array_flip($hind)); }, $fin);
-        
-        //     $total = count($fin);
-        //     $filt = $total;
-        
-        //     if ($search) {
-        //         $n = array();
-        //         foreach ($hind as $i) {
-        //             $tmp = array_filter($fin, function($row) use ($i, $search) {
-        //                 return (strpos(strtolower($row[$i]), $search) !== false);
-        //             });
-        //             $n = array_merge($n, array_keys($tmp));
-        //         }
-        //         $n = array_unique($n);
-        //         $fin = array_intersect_key($fin, array_flip($n));
-        //         $filt = count($fin);
-        //     }
-        
-        //     if ($filt > 0) {
-        //         usort($fin, function($a, $b) use ($sort_col, $sort_dir) {
-        //             $cmp = strcmp($a[$sort_col], $b[$sort_col]);
-        //             return ($sort_dir == 'asc') ? $cmp : -$cmp;
-        //         });
-        //         $fin = array_slice($fin, $start, $length);
-        //     } else {
-        //         $fin = array();
-        //     }
-        
-        //     $out = array(
-        //         "draw" => $draw,
-        //         "recordsTotal" => $total,
-        //         "recordsFiltered" => $filt,
-        //         "data" => $fin
-        //     );
-        //     return json_encode($out);
-        // }
+        // How to call:
+        // $filedir = Storage::path(config('app.jobdir') . '/jobs/' . $jobID . '/');
+        // return myFile::dt($filedir, $fin, $draw, $cols, $order_column, $order_dir, $start, $length, $search);
     }
 
     public function paramTable(Request $request)
     {
         $id = $request->input('id');
         $prefix = $request->input('prefix');
-        $filedir = config('app.jobdir') . '/' . $prefix . '/' . $id . '/';
-        $params = parse_ini_string(Storage::get($filedir . 'params.config'), false, INI_SCANNER_RAW);
-        $out = [];
-        foreach ($params as $key => $value) {
-            $out[] = [$key, $value];
-        }
-        return json_encode($out);
+        $file_path = config('app.jobdir') . '/' . $prefix . '/' . $id . '/' . 'params.config';
+
+        return myFile::parse_ini_file($file_path);
     }
 
     public function sumTable(Request $request)
     {
         $id = $request->input('id');
         $prefix = $request->input('prefix');
-        $filedir = config('app.temp_abs_path_to_jobs') . '/' . $prefix . '/' . $id . '/';
-        $table = '<table class="table table-bordered" style="width:auto;margin-right:auto; margin-left:auto; text-align: right;"><tbody>';
-        $lines = file($filedir . "summary.txt");
-        foreach ($lines as $l) {
-            $line = preg_split("/[\t]/", chop($l));
-            $table .= "<tr><td>" . $line[0] . "</td><td>" . $line[1] . "</td></tr>";
-        }
-        $table .= "</tbody></table>";
+        $file_path = config('app.jobdir') . '/' . $prefix . '/' . $id . '/' . 'summary.txt';
 
-        return $table;
+        return myFile::summary_table_in_html($file_path);
     }
-
-    public function manhattan($prefix, $id, $file)
-    {
-        $filedir = config('app.jobdir') . '/' . $prefix . '/' . $id . '/';
-
-        $f = $filedir . $file;
-        if ($file == "manhattan.txt") {
-            if (file_exists($f)) {
-                $file = fopen($f, 'r');
-                $header = fgetcsv($file, 0, "\t");
-                $all_rows = [];
-                while ($row = fgetcsv($file, 0, "\t")) {
-                    $row[0] = (int)$row[0];
-                    $row[1] = (int)$row[1];
-                    $row[2] = (float)$row[2];
-                    $all_rows[] = $row;
-                }
-                return json_encode($all_rows);
-            }
-        } else if ($file == "magma.genes.out") {
-            if (file_exists($f)) {
-                $file = fopen($f, 'r');
-                $header = fgetcsv($file, 0, "\t");
-                $all_rows = array();
-                while ($row = fgetcsv($file, 0, "\t")) {
-                    if ($row[1] == "X" | $row[1] == "x") {
-                        $row[1] = 23;
-                    }
-                    $row[1] = (int)$row[1];
-                    $row[2] = (int)$row[2];
-                    $row[3] = (int)$row[3];
-                    $row[8] = (float)$row[8];
-                    $all_rows[] = array($row[1], $row[2], $row[3], $row[8], $row[9]);
-                }
-                return json_encode($all_rows);
-            }
-        }
-    }
-
-    // deprecated to be removed
-    // public function QQplot(Request $request)
-    // {
-    //     $jobID = $request->input('jobID');
-    //     $fileNames = $request->input('fileNames');
-    //     $plot = $request->input('plot');
-    //     $filedir = config('app.jobdir') . '/jobs/' . $jobID . '/';
-
-    //     if (strcmp($plot, "Gene") == 0) {
-    //         $result = Helper::getFilesContents($filedir, $fileNames);
-    //         $data = $result[$fileNames[0]];
-
-    //         $obs = array();
-    //         $exp = array();
-    //         $c = 0;
-    //         foreach ($data as $row) {
-    //             $c++;
-    //             $obs[] = -log10($row["P"]);
-    //         }
-
-    //         sort($obs);
-    //         $step = (1 - 1 / $c) / $c;
-    //         $head = ["obs", "exp", "n"];
-    //         $all_row = array();
-    //         for ($i = 0; $i < $c; $i++) {
-    //             $all_row[] = array_combine($head, [$obs[$i], -log10(1 - $i * $step), $i + 1]);
-    //         }
-    //         return response()->json([$fileNames[0] => $all_row]);
-
-    //         // $file = $filedir . "magma.genes.out";
-    //         // if (Storage::exists($file)) {
-    //         //     $f = Storage::get($file);
-    //         //     $obs = array();
-    //         //     $exp = array();
-    //         //     $c = 0;
-    //         //     str_getcsv($f, "\t");
-    //         //     while ($row = str_getcsv($f, "\t")) {
-    //         //         $c++;
-    //         //         $obs[] = -log10($row[8]);
-    //         //     }
-    //         //     sort($obs);
-    //         //     $step = (1 - 1 / $c) / $c;
-    //         //     $head = ["obs", "exp", "n"];
-    //         //     $all_row = array();
-    //         //     for ($i = 0; $i < $c; $i++) {
-    //         //         $all_row[] = array_combine($head, [$obs[$i], -log10(1 - $i * $step), $i + 1]);
-    //         //     }
-    //         //     return response()->json($all_row);
-    //         // }
-    //     }
-    // }
-
-    // deprecated to be removed
-    // public function MAGMA_expPlot($prefix, $jobID)
-    // {
-    //     $filedir = config('app.jobdir') . '/' . $prefix . '/' . $jobID . '/';
-    //     $script = scripts_path('magma_expPlot.py');
-    //     $data = shell_exec("python $script $filedir");
-    //     return $data;
-    // }
 
     public function locusPlot(Request $request)
     {
         $jobID = $request->input('id');
-        $prefix = $request->input('prefix');
         $type = $request->input('type');
         $rowI = $request->input('rowI');
-        // $filedir = config('app.jobdir') . '/' . $prefix . '/' . $id . '/';
 
         $container_name = DockerNamesBuilder::containerName($jobID);
         $image_name = DockerNamesBuilder::imageName('laradock-fuma', 'locus_plot');
@@ -471,7 +275,7 @@ class FumaController extends Controller
             $zip->addFile($abs_path, $f);
         }
         $zip->close();
-        
+
         return response()->download(Storage::path($filedir . $zipfile))->deleteFileAfterSend(true);
     }
 
@@ -567,7 +371,7 @@ class FumaController extends Controller
         # create zip file and open it
         $zip = new \ZipArchive();
         $zip->open(Storage::path($zipfile), \ZipArchive::CREATE);
-                
+
         # add README file if exists in the public storage
         if (Storage::disk('public')->exists('README_g2f.txt')) {
             $zip->addFile(Storage::disk('public')->path('README_g2f.txt'), "README_g2f");
@@ -636,39 +440,13 @@ class FumaController extends Controller
         return response()->json([]);
     }
 
-    public function g2f_paramTable(Request $request)
-    {
-        $id = $request->input('id');
-        $prefix = $request->input('prefix');
-        $filedir = config('app.jobdir') . '/' . $prefix . '/' . $id . '/';
-
-        $params = parse_ini_string(Storage::get($filedir . 'params.config'), false, INI_SCANNER_RAW);
-        $out = [];
-        foreach ($params as $key => $value) {
-            $out[] = [$key, $value];
-        }
-        return json_encode($out);
-    }
-
     public function g2f_sumTable(Request $request)
     {
         $id = $request->input('id');
         $prefix = $request->input('prefix');
         $filedir = config('app.jobdir') . '/' . $prefix . '/' . $id . '/';
-        if ($prefix == "public") {
-            $filedir .= 'g2f/';
-        }
 
-        $out = [["No sumary table found.", "GENE2FUNC Job ID:" . $id]];
-        if (Storage::exists($filedir . "summary.txt")) {
-            $lines = file(Storage::path($filedir . "summary.txt"));
-            $out = [];
-            foreach ($lines as $l) {
-                $l = preg_split("/\t/", chop($l));
-                $out[] = [$l[0], $l[1]];
-            }
-        }
-        return json_encode($out);
+        return myFile::summary_table_in_json($filedir . "summary.txt");
     }
 
     public function expDataOption(Request $request)
