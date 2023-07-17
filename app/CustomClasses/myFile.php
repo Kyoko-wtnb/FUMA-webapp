d40e24554bd43dcfe6fff02c113f3aabbd06e17c
Anastasios Mitsigkolas <tasos1109@gmail.com>
2023-07-17 17:32:31 +0200

    Many commonly used functions across controllers have been moved here

--- app/CustomClasses/myFile.php
+++ app/CustomClasses/myFile.php
@@ -29,7 +29,6 @@ class myFile
 
         if (in_array($type, $acceptable_text_mime_types)) {
             Storage::put($filedir . '/' . $file_name, file_get_contents($file));
-
         } else if (in_array($type, $acceptable_zip_mime_types)) {
             Storage::put($filedir . '/' . 'temp', file_get_contents($file));
 
@@ -59,5 +58,153 @@ class myFile
             Storage::delete($filedir . '/temp');
         }
         return;
+    }
+
+    public static function processCsvDataWithHeaders(string $file_path, string $header)
+    {
+        $cols = explode(":", $header);
+        if (Storage::exists($file_path)) {
+            $file = fopen(Storage::path($file_path), 'r');
+            $all_rows = array();
+            $head = fgetcsv($file, 0, "\t");
+            $index = array();
+            foreach ($cols as $c) {
+                if (in_array($c, $head)) {
+                    $index[] = array_search($c, $head);
+                } else {
+                    $index[] = -1;
+                }
+            }
+            while ($row = fgetcsv($file, 0, "\t")) {
+                $temp = [];
+                foreach ($index as $i) {
+                    if ($i == -1) {
+                        $temp[] = "NA";
+                    } else {
+                        $temp[] = $row[$i];
+                    }
+                }
+                $all_rows[] = $temp;
+            }
+            $json = (array('data' => $all_rows));
+
+            return json_encode($json);
+        } else {
+            return json_encode(array('data' => []));
+        }
+    }
+
+    public static function csv_file_to_array(string $file_path)
+    {
+        $data = array();
+        $file = fopen($file_path, "r");
+        $header = fgetcsv($file, 0, "\t");
+
+        while ($row = fgetcsv($file, 0, "\t")) {
+            array_push($data, $row);
+        }
+
+        fclose($file);
+        return array("header" => $header, "data" => $data);
+    }
+
+    /**
+     * The following function (result of chatgpt) with some modifications could be used as a drop in replacement
+     * for the dt.py code and thus to replace the docker container.
+     */
+    public static function dt($filedir, $filename, $draw, $headers, $sort_col, $sort_dir, $start, $length, $search = null)
+    {
+        if ($search) {
+            $search = strtolower($search);
+        }
+
+        $cols = explode(":", $headers);
+        $fin = self::read_csv($filedir, $filename)["data"];
+        $header = self::read_csv($filedir, $filename)["header"];
+        $hind = array_map(function ($x) use ($header) {
+            return array_search($x, $header);
+        }, $cols);
+        $fin = array_map(function ($row) use ($hind) {
+            return array_intersect_key($row, array_flip($hind));
+        }, $fin);
+
+        $total = count($fin);
+        $filt = $total;
+
+        if ($search) {
+            $n = array();
+            foreach ($hind as $i) {
+                $tmp = array_filter($fin, function ($row) use ($i, $search) {
+                    return (strpos(strtolower($row[$i]), $search) !== false);
+                });
+                $n = array_merge($n, array_keys($tmp));
+            }
+            $n = array_unique($n);
+            $fin = array_intersect_key($fin, array_flip($n));
+            $filt = count($fin);
+        }
+
+        if ($filt > 0) {
+            usort($fin, function ($a, $b) use ($sort_col, $sort_dir) {
+                $cmp = strcmp($a[$sort_col], $b[$sort_col]);
+                return ($sort_dir == 'asc') ? $cmp : -$cmp;
+            });
+            $fin = array_slice($fin, $start, $length);
+        } else {
+            $fin = array();
+        }
+
+        $out = array(
+            "draw" => $draw,
+            "recordsTotal" => $total,
+            "recordsFiltered" => $filt,
+            "data" => $fin
+        );
+        return json_encode($out);
+    }
+
+    public static function summary_table_in_json(string $file_path)
+    {
+        if (Storage::exists($file_path)) {
+            $lines = file(Storage::path($file_path));
+            $out = [];
+            foreach ($lines as $l) {
+                $l = preg_split("/\t/", chop($l));
+                $out[] = [$l[0], $l[1]];
+            }
+            return json_encode($out);
+        } else {
+            return json_encode(['error' => 'No summary table found.']);
+        }
+    }
+
+    public static function summary_table_in_html(string $file_path)
+    {
+        if (Storage::exists($file_path)) {
+            $table = '<table class="table table-bordered" style="width:auto;margin-right:auto; margin-left:auto; text-align: right;"><tbody>';
+            $lines = file(Storage::path($file_path));
+            foreach ($lines as $l) {
+                $line = preg_split("/[\t]/", chop($l));
+                $table .= "<tr><td>" . $line[0] . "</td><td>" . $line[1] . "</td></tr>";
+            }
+            $table .= "</tbody></table>";
+            return $table;
+        } else {
+            return '<p>No summary table found.</p>';
+        }
+    }
+
+    public static function parse_ini_file(string $file_path)
+    {
+        if (Storage::exists($file_path)) {
+            $params = parse_ini_string(Storage::get($file_path), false, INI_SCANNER_RAW);
+            $out = [];
+            foreach ($params as $key => $value) {
+                $out[] = [$key, $value];
+            }
+            return json_encode($out);
+        } else {
+            return json_encode(['error' => 'No params.config file found.']);
+        }
     }
 }
