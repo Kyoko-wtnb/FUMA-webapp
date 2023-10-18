b09b8ba93acff206705b6fd99f11182f195b50cf
Anastasios Mitsigkolas <tasos1109@gmail.com>
2023-10-18 10:37:53 +0200

    Adding multiple child capability to jobs
    Missing dependency

--- app/Models/SubmitJob.php
+++ app/Models/SubmitJob.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 use Illuminate\Database\Eloquent\Model;
 
