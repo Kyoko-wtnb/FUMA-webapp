d72823635bf041c0a0d04bb3583585c777761d63
Anastasios Mitsigkolas <tasos1109@gmail.com>
2023-10-23 17:41:49 +0200

    Basic mail for completed and failed with err code jobs

--- app/Helpers/JobHelper.php
+++ app/Helpers/JobHelper.php
@@ -4,25 +4,19 @@ namespace App\Helpers;
 
 use Illuminate\Support\Facades\App;
 use App\Models\SubmitJob;
+use App\Mail\JobCompletedSuccessfully;
+use App\Mail\JobFailedWithErrorCode;
 use Mail;
 
 
 class JobHelper
 {
-    public static function sendJobMail($user, $job, $subject, $msg)
+    public static function sendJobMail($job, $mailer)
     {
         if (App::isProduction()) {
-            $data = [
-                'jobID' => $job->jobID,
-                'jobtitle' => $job->title,
-                'status' => $$job->status,
-                'msg' => $msg
-            ];
             try {
-                Mail::send('emails.jobComplete', $data, function ($m) use ($user, $subject) {
-                    $m->from('noreply@ctglab.nl', "FUMA web application");
-                    $m->to($user->email, $user->name)->subject($subject);
-                });
+                Mail::to($job->user->email, $job->user->name)
+                    ->send($mailer);
                 return true;
             } catch (Throwable $e) {
                 return false;
@@ -45,14 +39,10 @@ class JobHelper
 
         if (config('snp2gene_status_codes.' . $report_code . '.type') == 'err') {
             // error occured
-
-            // Comment out if you wanna delete the db entry and/or directory
-            // $job->delete();
-            // Storage::deleteDirectory(config('app.jobdir') . '/jobs/' . $jobID);
-            JobHelper::sendJobMail($job->user, $job, 'FUMA job failed', $msg);
+            JobHelper::sendJobMail($job, new JobFailedWithErrorCode($job, $msg));
         } elseif (config('snp2gene_status_codes.' . $report_code . '.type') == 'success') {
             // success
-            JobHelper::sendJobMail($job->user, $job, 'FUMA your job has been completed', $msg);
+            JobHelper::sendJobMail($job, new JobCompletedSuccessfully($job, $msg));
         }
         return;
     }
---
+++ app/Mail/JobCompletedSuccessfully.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+use App\Models\SubmitJob;
+
+class JobCompletedSuccessfully extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(protected SubmitJob $job, protected string $msg)
+    {
+        //
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Job Completed Successfully',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'emails.jobComplete',
+            with: [
+                'jobID' => $this->job->jobID,
+                'jobtitle' => $this->job->title,
+                'status' => $this->job->status,
+                'msg' => $this->msg,
+            ],
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
